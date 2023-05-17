6ab1c270b1ab69dbd9b05eb6a748b3cf998ec451
jechus <jesuse2409@gmail.com>
2023-05-17 18:19:16 -0400

    NEW: Primera Implementacion de Swagger

--- src/main/java/com/crud/tarjeta/api_tarjetas/ApiTarjetasApplication.java
+++ src/main/java/com/crud/tarjeta/api_tarjetas/ApiTarjetasApplication.java
@@ -1,12 +1,16 @@
 package com.crud.tarjeta.api_tarjetas;
 
+import com.crud.tarjeta.api_tarjetas.configuration.SwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(SwaggerConfig.class)
 public class ApiTarjetasApplication {
 
 	public static void main(String[] args) {
+		//Class<?>[] configClasses = {ApiTarjetasApplication.class, SwaggerConfig.class};
 		SpringApplication.run(ApiTarjetasApplication.class, args);
 	}
 
